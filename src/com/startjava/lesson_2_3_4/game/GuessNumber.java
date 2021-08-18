2b9b5c8076ca0545603a05e22b178af778da4b18
Evgen-Mutagen <jeka190@mail.ru>
2021-08-18 22:35:41 +0300

    Upload HW

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -7,10 +7,8 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private int i;
-    private int[] number1 = new int[10];
-    private int[] number2 = new int[10];
-
-    Player array = new Player(number1, number2);
+    int number1;
+    int number2;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -20,17 +18,15 @@ public class GuessNumber {
     public void start() {
         Scanner scan = new Scanner(System.in);
         int secretNumber = (int) (Math.random() * 101);
-
         for (i = 0; i < 10; i++) {
             System.out.println("игрок " + player1.getName() + " введите число : ");
-            number1[i] = scan.nextInt();
-            if (number1[i] == secretNumber) {
+            number1 = scan.nextInt();
+            if (number1 == secretNumber) {
                 System.out.println("Победил " + player1.getName() + " с " + (i + 1) + " попытки" + " Это было число - " + secretNumber);
-                i++;
                 break;
-            } else if (number1[i] < secretNumber) {
+            } else if (number1 < secretNumber) {
                 System.out.println("Число " + player1.getName() + " меньше числа компьютера");
-            } else if (number1[i] > secretNumber) {
+            } else if (number1 > secretNumber) {
                 System.out.println("Число " + player1.getName() + " больше числа компьютера");
             }
             if (i == 9) {
@@ -38,14 +34,13 @@ public class GuessNumber {
             }
 
             System.out.println("игрок " + player2.getName() + " введите число : ");
-            number2[i] = scan.nextInt();
-            if (number2[i] == secretNumber) {
+            number2 = scan.nextInt();
+            if (number2 == secretNumber) {
                 System.out.println("Победил " + player2.getName() + " с " + (i + 1) + " попытки" + " Это было число - " + secretNumber);
-                i++;
                 break;
-            } else if (number2[i] < secretNumber) {
+            } else if (number2 < secretNumber) {
                 System.out.println("Число " + player2.getName() + " меньше числа компьютера");
-            } else if (number2[i] > secretNumber) {
+            } else if (number2 > secretNumber) {
                 System.out.println("Число " + player2.getName() + " больше числа компьютера");
             }
             if (i == 9) {
@@ -56,7 +51,7 @@ public class GuessNumber {
 
     public void copyOfNumber1() {
         System.out.print("Числа игрока " + player1.getName() + ": ");
-        int[] number1Copy = Arrays.copyOf(array.getNumber1(), i);
+        int[] number1Copy = Arrays.copyOf(array1.getNumber(), i);
         for (int k : number1Copy) {
             System.out.print(k + " ");
         }
@@ -65,7 +60,7 @@ public class GuessNumber {
 
     public void copyOfNumber2() {
         System.out.print("Числа игрока " + player2.getName() + ": ");
-        int [] number2Copy = Arrays.copyOf(array.getNumber2(), i);
+        int [] number2Copy = Arrays.copyOf(array2.getNumber(), i);
         for (int k : number2Copy) {
             System.out.print(k + " ");
         }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -1,11 +1,8 @@
 package com.startjava.lesson_2_3_4.game;
 
-import java.util.Arrays;
-
 public class Player {
     private String name;
-    private int[] number1;
-    private int[] number2;
+    private int[] number = new int[10];
 
     public Player(String name) {
 
@@ -17,16 +14,10 @@ public class Player {
         return name;
     }
 
-    public Player(int[] number1, int[] number2) {
-        this.number1 = number1;
-        this.number2 = number2;
-    }
 
-    public int[] getNumber1() {
-        return number1;
-    }
 
-    public int[] getNumber2() {
-        return number2;
-    }
+
+
+
+
 }
