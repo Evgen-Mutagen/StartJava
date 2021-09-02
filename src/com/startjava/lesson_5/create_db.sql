457481d77ee53f8943089978d0fd883c23a22c9e
Evgen-Mutagen <jeka190@mail.ru>
2021-09-02 19:09:12 +0300

    Upload HW_5

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE Jaegers;	
+	
+\c jaegers;	
+	
+CREATE TABLE Jaegers (	
+id	  SERIAL PRIMARY KEY,
+modelName TEXT,
+mark	  CHAR(50),
+height	  INTEGER,
+weight	  INTEGER,
+status	  CHAR(50),
+origin    TEXT,
+launch    DATA,
+kaijuKill INTEGER);
---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill)
+VALUES	('Crimson Typhoon',	'Mark-4',	'250',	'1722',	'Destroyed',	'China',			'2018-08-22',	'7'),
+	('Gipsy Danger',	'Mark-3',	'260',	'1980',	'Destroyed',	'United States of America',	'2017-07-10',	'9'),
+	('Striker Eureka',	'Mark-5',	'250',	'1850',	'Destroyed',	'Australia',			'2019-05-02',	'11'),
+	('Cherno Alpha',	'Mark-1',	'280',	'2412',	'Destroyed',	'Russia',			'2015-05-05',	'6'),
+	('Gipsy Avenger',	'Mark-6',	'268',	'2004',	'Destroyed',	'United States of America',	'2034-01-12',	'1'),
+	('Guardian Bravo',	'Mark-6',	'240',	'1975',	'Active',	'Japan',			'2033-12-15',	'3'),
+	('Storm Garuda',	'Mark-7',	'255',	'1995',	'Active',	'Japan',			'2034-02-07',	'2'),
+	('November Ajax',	'Mark-5',	'232',	'2128',	'Destroyed',	'China',			'2025-05-01',	'4'),
+	('Bracer Phoenix',	'Mark-6',	'259',	'2223',	'Destroyed',	'Russia',			'2031-07-23',	'3'),
+	('Titan Redeemer',	'Mark-6',	'242',	'2134',	'Destroyed',	'United States of America',	'2029-04-19',	'2');
